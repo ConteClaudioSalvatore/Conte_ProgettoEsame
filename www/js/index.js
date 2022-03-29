6258ef0c91fb0bd4a1e71dd64aa4a8f8b8352225
ConteClaudioSalvatore <claudioconte51@gmail.com>
2022-03-29 15:01:16 +0200

    s

--- www/js/index.js
+++ www/js/index.js
@@ -19,66 +19,91 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 let map;
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    mappa();
-    $("#btnBarcode").on("click", barcodeScanner);
-    $("#txtSearchBox").on("focus", suggerimenti);
-    StatusBar.show();
+  // Cordova is now initialized. Have fun!
+  mappa();
+  $("#btnBarcode").on("click", barcodeScanner);
+  $("#txtSearchBox").on("focus", suggerimenti);
+  StatusBar.show();
 }
 
-function barcodeScanner(){
+function barcodeScanner() {
   cordova.plugins.barcodeScanner.scan(
     mostraProdotto,
     function (error) {
-        alert("Scanning failed: " + error);
+      alert("Scanning failed: " + error);
     },
     {
-        preferFrontCamera : false, // iOS and Android
-        showFlipCameraButton : true, // iOS and Android
-        showTorchButton : true, // iOS and Android
-        torchOn: false, // Android, launch with the torch switched on (if available)
-        saveHistory: true, // Android, save scan history (default false)
-        prompt : "Scannerizza il codice a barre pagliaccio", // Android
-        resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-        formats : "EAN_13", // default: all but PDF_417 and RSS_EXPANDED
-        orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-        disableAnimations : true, // iOS
-        disableSuccessBeep: false // iOS and Android
+      preferFrontCamera: false, // iOS and Android
+      showFlipCameraButton: true, // iOS and Android
+      showTorchButton: true, // iOS and Android
+      torchOn: false, // Android, launch with the torch switched on (if available)
+      saveHistory: true, // Android, save scan history (default false)
+      prompt: "Scannerizza il codice a barre pagliaccio", // Android
+      resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+      formats: "EAN_13", // default: all but PDF_417 and RSS_EXPANDED
+      orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+      disableAnimations: true, // iOS
+      disableSuccessBeep: false, // iOS and Android
     }
- );
+  );
 }
 
-function mappa(){
-    $.ajax({
-        url: "https://nominatim.openstreetmap.org/search?format=json&city=Fossano",
-        //data:"{ut:'"+user+"', pwd='"+md5(pwd)+"'}",
-        type: 'GET',
-        success: function (data) {
-          console.log(data);
-          let coord = [7.7362988,44.5550253];
-          console.log(coord);
-          //Visualizzo la mappa centrata su narzole
+function mappa() {
+  $.ajax({
+    url: "https://nominatim.openstreetmap.org/search?format=json&city=Fossano",
+    //data:"{ut:'"+user+"', pwd='"+md5(pwd)+"'}",
+    type: "GET",
+    success: function (data) {
+      console.log(data);
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          //prendo le coordinate dell'utente
+          let coords = [position.coords.longitude, position.coords.latitude];
+          console.log(coords);
+          //Visualizzo la mappa centrata sulla mia posizione attuale
           map = new ol.Map({
-            target: 'map',
+            target: "map",
             layers: [
               new ol.layer.Tile({
-                source: new ol.source.OSM()
-              })
+                source: new ol.source.OSM(),
+              }),
             ],
             view: new ol.View({
-              center: ol.proj.fromLonLat(coord),
-              zoom: 17
-            })
+              center: ol.proj.fromLonLat(coords),
+              zoom: 17,
+            }),
           });
+          //Aggiungo un marker sulla mappa
+          let layerMarker = new ol.layer.Vector({
+            source: new ol.source.Vector({
+              features: [
+                new ol.Feature({
+                  geometry: new ol.geom.Point(ol.proj.fromLonLat(coords)),
+                }),
+              ],
+            }),
+          })
+          map.addLayer(layerMarker);
         },
-        error: function(e) {
-            alert('Error: '+e);
-        }  
-    });
+        function (error) {
+          console.log(error);
+          navigator.notification.alert(
+            "Errore nel recupero della posizione\nProva a riavviare l'applicazione o accettare i permessi di localizzazione",
+            null,
+            "Errore",
+            "OK"
+          );
+        },
+        { enableHighAccuracy: true }
+      );
+    },
+    error: function (e) {
+      alert("Error: " + e);
+    },
+  });
 }
-
-
+function suggerimenti() {}
