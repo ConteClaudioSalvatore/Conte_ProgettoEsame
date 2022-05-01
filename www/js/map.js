ddfbf5fe455535c45a96963d895547c0413c7673
ConteClaudioSalvatore <claudioconte51@gmail.com>
2022-05-02 00:50:55 +0200

    aggiunta di marker sulle posizioni dei supermercati trovati dall'api di openstreetmap, risolte alcune problematiche

--- www/js/map.js
+++ www/js/map.js
@@ -1,9 +1,12 @@
 let coords;
 let mapPinned = true;
 let map;
+let layerUserMarker;
 let positionTracking;
+let supermarketInfo;
 let marker;
 function mappa() {
+  $(".mapOrientation").hide();
   $.ajax({
     url: "https://nominatim.openstreetmap.org/search?format=json&countrycodes=it",
     type: "GET",
@@ -32,15 +35,22 @@ function mappa() {
           });
           marker.setId("centralPoint");
           //Aggiungo un marker sulla mappa
-          layerMarker = new ol.layer.Vector({
+          layerUserMarker = new ol.layer.Vector({
             source: new ol.source.Vector({
               features: [
                 marker
               ],
             }),
           });
-          map.addLayer(layerMarker);
+          map.addLayer(layerUserMarker);
+          $(".mapOrientation").on("click", function () {
+            map.getView().setRotation(0);
+            $(".mapOrientation").fadeOut(200);
+          });
+          //Aggiungo un pulsante per la posizione attuale
           attachMapToCurrentPosition();
+          //aggiungo i marker dei supermarket nella città
+          addSupermarketMarkers(map);
         },
         function (error) {
           console.log(error);
@@ -60,6 +70,8 @@ function mappa() {
   });
 }
 function onMapMove() {
+  if(map.getView().getRotation()!=0)
+      $(".mapOrientation").fadeIn(200);
   if(
     (coords[1]*1000 < map.getView().getCenter()[1]*1000 || 
     coords[1]*1000 > map.getView().getCenter()[1]*1000 || 
@@ -73,11 +85,10 @@ function onMapMove() {
   }  
 }
 function onGpsButtonClick() {
-  
   btnGps.fadeOut(200);
   map.removeEventListener('movestart');
   map.getView().setCenter(ol.proj.fromLonLat(coords));
-  map.on("movestart", onMapMove);
+  map.addEventListener("movestart", onMapMove);
   mapPinned = true;
   attachMapToCurrentPosition();
 }
@@ -87,9 +98,11 @@ function attachMapToCurrentPosition() {
       coords = [position.coords.longitude, position.coords.latitude];
       //marker.getGeometry().setCoordinates(coords);
       map.removeEventListener('movestart');
-      if(mapPinned)
+      if(mapPinned){
         map.getView().setCenter(ol.proj.fromLonLat(coords));
-      map.on("movestart", onMapMove);
+        layerUserMarker.getSource().getFeatures()[0].getGeometry().setCoordinates(ol.proj.fromLonLat(coords));
+      }
+      map.addEventListener("movestart", onMapMove);
     },
     (error) => {
       console.log(error);
@@ -103,5 +116,66 @@ function attachMapToCurrentPosition() {
     { enableHighAccuracy: true, timeout: 3000, maximumAge: 3000 }
   );
 }
-
+function addSupermarketMarkers(map){
+  //prendo la città dalle coordinate dell'utente
+  $.ajax({
+    url: "https://nominatim.openstreetmap.org/reverse?format=json&lat="+coords[1]+"&lon="+coords[0],
+    type: "GET",
+    success: function (data) {
+      let posInfo = {
+        cityName :data.address.city,
+        village : data.address.village,
+        regione : data.address.state
+      }
+      
+      console.log("regione: " + posInfo.regione);
+      console.log("città: " + posInfo.cityName);
+      console.log("paese: " + posInfo.village);
+      getSupermarkets(posInfo.cityName, posInfo.regione, posInfo.village).then(function (sm) {
+        var vectorSource = new ol.source.Vector({
+          features: []
+        });
+        sm.forEach(supermarket => {
+          console.log("supermarket: " , supermarket);
+          var markerStyle = new ol.style.Icon(({
+            src: 'img/shop.png',
+          }));
+          let marker = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([supermarket.lon, supermarket.lat]))
+          });
+          marker.setStyle(new ol.style.Style({
+            image: markerStyle,
+          }));
+          vectorSource.addFeature(marker);
+        }) 
+        console.log(vectorSource);
+        var markerLayer = new ol.layer.Vector({
+          visible: true,
+          source: vectorSource
+        });
+        map.addLayer(markerLayer);
+      });
+    },
+    error: function (e) {
+      alert("Error: " + e);
+    }
+  });
+}
+async function getSupermarkets(cityName, regione, village){
+  let url = "https://nominatim.openstreetmap.org/search?format=json&countrycodes=it&q="
+  if(village!=undefined){
+    url+=village.split(' ').join('+');
+  }
+  else{
+    url+=cityName.split(' ').join('+');
+  }
+  url+="+supermercato&state="+regione.split(' ').join('+');
+  return await $.ajax({
+    url: url,
+    type: "GET",
+    error: function (e) {
+      alert("Error: " + e);
+    }
+  })
+}
 
